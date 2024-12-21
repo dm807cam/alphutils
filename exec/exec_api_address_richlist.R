2581958d9a7c3c6d6c837bcd4fa675431c135703
camwod <dm807@cantab.ac.uk>
2024-12-21 09:10:43 +0100

    Create exec_api_address_richlist.R

---
+++ exec/exec_api_address_richlist.R
@@ -0,0 +1,18 @@
+pkgload::load_all()
+
+utc_time <- utc_time()
+dt <- api_address_richlist(min_balance = 10000L)
+dt[, datetime := utc_time]
+
+path <- fs::path(fs::path_home(),
+                 "alephium_richlist")
+
+if(!fs::dir_exists(path)) {
+  fs::dir_create(path)
+}
+
+file_path = fs::path(path,
+                     as.numeric(utc_time),
+                     ext = "csv")
+
+write.csv(dt, file = file_path)
